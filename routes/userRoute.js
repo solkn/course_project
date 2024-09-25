16b39be0ed8c9a2ecd7d81106073ebcf5bbd8156
solkn <solomonkindie52@gmail.com>
2024-09-25 18:20:56 +0300

    token

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const authenticateToken = require('../middleware/authMiddleware'); // Import auth middleware
 
 const router = express.Router();
 
-router.post('/:user_id/courses', userController.addCourseToUser);
+router.post('/:user_id/courses', authenticateToken, userController.addCourseToUser);
 
-router.get('/:user_id/courses/progress', userController.getUserCourseProgress);
+router.get('/:user_id/courses/progress', authenticateToken, userController.getUserCourseProgress);
 
-router.put('/:user_id/courses/:course_id/progress', userController.updateUserCourseProgress);
+router.put('/:user_id/courses/:course_id/progress', authenticateToken, userController.updateUserCourseProgress);
 
 module.exports = router;
